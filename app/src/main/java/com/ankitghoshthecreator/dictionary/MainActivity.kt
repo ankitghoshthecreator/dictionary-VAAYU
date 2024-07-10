dc4b5770fd404c46f3aab78813dce7abda459c1c
ankitghoshthecreator <baruipara01@gmail.com>
2024-07-10 21:52:45 +0530

    api set up complete and working properly

--- app/src/main/java/com/ankitghoshthecreator/dictionary/MainActivity.kt
+++ app/src/main/java/com/ankitghoshthecreator/dictionary/MainActivity.kt
@@ -1,15 +1,36 @@
 package com.ankitghoshthecreator.dictionary
 
+import android.os.Binder
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ankitghoshthecreator.dictionary.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.searchBtn.setOnClickListener {
+            val word = binding.searchInput.text.toString()
+            getMeaning(word)
+        }
+
+    }
+
+    private fun getMeaning(word: String){
+        GlobalScope.launch {
+           val response= Retrofitinstance.dictionaryApi.getMeaning(word)
 
         }
+
     }
+}
--- app/src/main/java/com/ankitghoshthecreator/dictionary/Retrofitinstance.kt
+++ app/src/main/java/com/ankitghoshthecreator/dictionary/Retrofitinstance.kt
@@ -1,4 +1,18 @@
 package com.ankitghoshthecreator.dictionary
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
 object Retrofitinstance {
+    private const val BASE_URL = "https://api.dictionaryapi.dev/api/v2/entries/"
+
+    private fun getInstance(): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val dictionaryApi : DictionaryApi = getInstance().create(DictionaryApi::class.java)
 }
